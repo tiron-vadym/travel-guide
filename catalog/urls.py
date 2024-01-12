1353ff0c68f3657870702872fd88e5bbe965cfdd
Vadym Tiron <tironvadim1@gmail.com>
2024-01-12 15:28:33 +0200

    Create home page with url, view and templates

---
+++ catalog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from catalog import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+]
+
+app_name = "catalog"
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from .models import User, City, Review, Route
 
-# Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_users = User.objects.count()
+    num_cities = City.objects.count()
+    num_reviews = Review.objects.count()
+    num_routes = Route.objects.count()
+
+    context = {
+        "num_users": num_users,
+        "num_cities": num_cities,
+        "num_reviews": num_reviews,
+        "num_routes": num_routes,
+    }
+    return render(request, "catalog/index.html", context=context)
