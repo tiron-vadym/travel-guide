fa6c57216a45c0fb74b292b6094d5b0dabd055f9
Vadym Tiron <tironvadim1@gmail.com>
2024-01-12 14:23:39 +0200

    Create admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth import UserAdmin as BaseUserAdmin
+from .models import User, City, Review, Route
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ("username", "email", "password", "first_name", "last_name")
+
+
+class CityAdmin(admin.ModelAdmin):
+    pass
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    pass
+
+
+class RouteAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(City, CityAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Route, RouteAdmin)
