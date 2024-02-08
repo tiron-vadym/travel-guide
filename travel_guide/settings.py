f3ed106b305079ebbc49e3a4cf4f0d6f1cf1af98
Vadym Tiron <tironvadim1@gmail.com>
2024-02-08 17:37:47 +0200

    Update STRATIC_URL and STATIC_ROOT

--- travel_guide/settings.py
+++ travel_guide/settings.py
@@ -126,9 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
-STATIC_ROOT = "staticfiles/"
+STATIC_ROOT = "/staticfiles/"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
